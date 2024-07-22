9df37fc2ba7f57cc4185e7a998b14816ab09aaef
Antonio Campos <antoniomallmann01@gmail.com>
2024-07-22 11:17:13 -0300

    Criaçao de construtor e getters e setters na classe Cliente.

--- src/poo/Cliente.java
+++ src/poo/Cliente.java
@@ -7,7 +7,44 @@ public class Cliente {
     private Endereco endereco;
     private int telefone;
 
+    public Cliente(String nome, String email, Endereco endereco, int telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
 
     @Override
     public String toString() {
