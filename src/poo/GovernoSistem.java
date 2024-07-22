95e998742205f73791e33f445896e450d5bbb57f
antoniomallmann <antoniomallmann01@gmail.com>
2024-07-22 19:46:08 -0300

    implementação do método de cadastro.

--- src/poo/GovernoSistem.java
+++ src/poo/GovernoSistem.java
@@ -2,12 +2,32 @@ package poo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GovernoSistem {
 
     Cliente cliente = new Cliente();
-    Endereco endereco = new Endereco();
     List<Cliente> listaDeClientes = new ArrayList<>();
 
+    public Cliente cadastrarCliente(Scanner scanner) {
+        System.out.println("Informe seu nome: ");
+        String nome = scanner.nextLine();
 
+        System.out.println("Informe seu melhor email: ");
+        String email = scanner.nextLine();
+
+        System.out.println("Digite seu endereco: ");
+        System.out.println("informe o estado: ");
+        String estado = scanner.nextLine();
+        System.out.println("Informe a cidade: ");
+        String cidade = scanner.nextLine();
+        System.out.println("Informe o bairro: ");
+        String bairro = scanner.nextLine();
+
+        System.out.println("Informe seu número de telefone: ");
+        int telefone = scanner.nextInt();
+        scanner.nextLine();
+
+        return new Cliente(nome,email,new Endereco(estado,cidade,bairro),telefone);
+    }
 }
