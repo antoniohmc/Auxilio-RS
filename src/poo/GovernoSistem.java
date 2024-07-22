d8b7df6bf4fe02bf6ac14842605b57ca41c13daf
antoniomallmann <antoniomallmann01@gmail.com>
2024-07-22 19:32:08 -0300

    Criação da classe GovernoSistem.

---
+++ src/poo/GovernoSistem.java
@@ -0,0 +1,13 @@
+package poo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GovernoSistem {
+
+    Cliente cliente = new Cliente();
+    Endereco endereco = new Endereco();
+    List<Cliente> listaDeClientes = new ArrayList<>();
+
+
+}
