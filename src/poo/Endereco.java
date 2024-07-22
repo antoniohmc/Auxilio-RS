4baf5956b31572223a5ee8b72f1912c173a49c15
antoniomallmann <antoniomallmann01@gmail.com>
2024-07-22 19:31:40 -0300

    Criação da classe Endereco.

---
+++ src/poo/Endereco.java
@@ -0,0 +1,50 @@
+package poo;
+
+public class Endereco {
+
+    private String estado;
+    private String cidade;
+    private String bairro;
+
+    public Endereco() {
+    }
+
+    public Endereco(String estado, String cidade, String bairro) {
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+            "estado='" + estado + '\'' +
+            ", cidade='" + cidade + '\'' +
+            ", bairro='" + bairro + '\'' +
+            '}';
+    }
+}
