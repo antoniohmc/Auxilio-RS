e50d8572290d00764d9bfb4c9c26d42deb70c48d
Antonio Campos <antoniomallmann01@gmail.com>
2024-07-24 00:52:33 -0300

    Finalização da primeira etapa do projeto.

--- src/poo/Menu.java
+++ src/poo/Menu.java
@@ -37,7 +37,7 @@ public class Menu {
                     break;
 
                 case 3:
-                    governoSistem.validarAuxilio();
+                    governoSistem.validarAuxilio(scanner);
                     break;
 
                 default:
